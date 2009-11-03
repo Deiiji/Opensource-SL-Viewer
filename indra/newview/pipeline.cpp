a811435637313089a95d1a41d553cc6c5f9ce338
soft.linden <soft.linden@a6265765-422e-0410-accc-9fb60e44a920>
2009-11-03 22:28:53 +0000

    VWR-4232 Some particles do not disappear when UI is hidden

--- indra/newview/pipeline.cpp
+++ indra/newview/pipeline.cpp
@@ -2720,12 +2720,20 @@ void LLPipeline::renderGeom(LLCamera& camera, BOOL forceVBOUpdate)
 
 	LLVertexBuffer::unbind();
 	
-	if (!LLPipeline::sReflectionRender && !LLPipeline::sRenderDeferred && gPipeline.hasRenderDebugFeatureMask(LLPipeline::RENDER_DEBUG_FEATURE_UI))
+	if (!LLPipeline::sReflectionRender && !LLPipeline::sRenderDeferred)
 	{
-		// Render debugging beacons.
-		gObjectList.renderObjectBeacons();
-		LLHUDObject::renderAll();
-		gObjectList.resetObjectBeacons();
+		if (gPipeline.hasRenderDebugFeatureMask(LLPipeline::RENDER_DEBUG_FEATURE_UI))
+		{
+			// Render debugging beacons.
+			gObjectList.renderObjectBeacons();
+			LLHUDObject::renderAll();
+			gObjectList.resetObjectBeacons();
+		}
+		else
+		{
+			// Make sure particle effects disappear
+			LLHUDObject::renderAll();
+		}
 	}
 
 	LLAppViewer::instance()->pingMainloopTimeout("Pipeline:RenderGeomEnd");
@@ -2949,6 +2957,11 @@ void LLPipeline::renderGeomPostDeferred(LLCamera& camera)
 		gObjectList.renderObjectBeacons();
 		LLHUDObject::renderAll();
 		gObjectList.resetObjectBeacons();
+	}
+	else
+	{
+		// Make sure particle effects disappear
+		LLHUDObject::renderAll();
 	}
 
 	if (occlude)
