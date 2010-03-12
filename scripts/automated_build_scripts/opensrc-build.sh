eb14717e8a4c8a74536ccf2d537e9aa520344b28
merov.linden <merov.linden@a6265765-422e-0410-accc-9fb60e44a920>
2010-03-12 02:59:15 +0000

    Roll back of svn rev 3225 (wrong way to fix the problem)

--- scripts/automated_build_scripts/opensrc-build.sh
+++ scripts/automated_build_scripts/opensrc-build.sh
@@ -135,6 +135,9 @@ trap fail 1 2 3 14 15
 # Check location
 cd "$here/../.."
 
+test -x ../linden/scripts/automated_build_scripts/opensrc-build.sh\
+ || fail 'The parent dir of your checkout needs to be named "linden"' 
+
 . doc/asset_urls.txt
 get_asset "$SLASSET_ART"
 
@@ -372,7 +375,7 @@ do
   fi
 done
 
-# check status and upload results to S3
+# check statuis and upload results to S3
 subject=
 if $succeeded
 then
