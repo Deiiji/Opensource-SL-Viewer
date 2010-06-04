036cf51ad80ca3d81078f5f4e02acc4cec918f82
thickbrick.sleaford <thickbrick.sleaford@a6265765-422e-0410-accc-9fb60e44a920>
2010-06-04 22:32:19 +0000

    SNOW-636: Parse links to maps.secondlife.com in chat as slurls.

--- indra/llui/lltexteditor.cpp
+++ indra/llui/lltexteditor.cpp
@@ -4518,6 +4518,12 @@ BOOL LLTextEditor::findHTML(const std::string &line, S32 *begin, S32 *end) const
 		
 		if (strpos < 0)
 		{
+			slurlID="maps.secondlife.com/secondlife/";
+			strpos = url.find(slurlID);
+		}
+	
+		if (strpos < 0)
+		{
 			slurlID="secondlife://";
 			strpos = url.find(slurlID);
 		}
